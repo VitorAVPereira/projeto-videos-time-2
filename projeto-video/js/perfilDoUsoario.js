0679cb35afcb1c992adf07df8dced63d5c26a2cd
sareh65 <sareh.na65@gmail.com>
2022-05-12 15:53:24 -0300

    local storage

--- projeto-video/js/perfilDoUsoario.js
+++ projeto-video/js/perfilDoUsoario.js
@@ -1,31 +1,54 @@
 
-console.log('Testando JS')
-
-//escopos var let const
-let videos='{"tipo":"Site de Assinatura","nome":"Sareh na","idade":35,"email":"sareh@gmail.com","preferencia":"Comedia","dataInicio":"05/04/2022","tipoAssinatura":"Gold","valor":30}'
-
-console.log(videos)
+let videos='{"tipo":"Bem vinda!","idade":35,"preferencia":"Comedia","dataInicio":"05/04/2022","tipoAssinatura":"Gold","valor":30}'
 let videosJSON= JSON.parse(videos)
-
-console.log(videosJSON)
-
 let conteudo=document.querySelector("#conteudo")
 let h3=document.querySelector("#conteudo h3")
-let p1=document.querySelector("#p1")
-let p2=document.querySelector("#p2")
-let p3=document.querySelector("#p3")
-let p4=document.querySelector("#p4")
-let p5=document.querySelector("#p5")
-let p6=document.querySelector("#p6")
-let p7=document.querySelector("#p7")
+let idadeUsu=document.querySelector("#idadeUsu")
+let preferenciaUsu=document.querySelector("#preferenciaUsu")
+let dataUso=document.querySelector("#dataUso")
+let tipoUsu=document.querySelector("#tipoUsu")
+let valorUsu=document.querySelector("#valorUsu")
+let nome=localStorage.nomeUser
+
+let nomeUsu=document.querySelector("#nomeUsu")
+    
+    nomeUsu.innerHTML='Nome : '+ nome
+
+let email=localStorage.emailUser
+let emailUsu=document.querySelector("#emailUsu")
+ 
+    emailUsu.innerHTML='Email : '+ email
+    
+
+let tel=localStorage.telefoneUser
+let telUsu=document.querySelector("#telUsu")
+
+    
+    telUsu.innerHTML='Telefone : '+ tel
+    
+
+
+
+
 
 h3.innerHTML=videosJSON.tipo
-p1.innerHTML="Nome: "+videosJSON.nome
-p2.innerHTML="Idade: "+videosJSON.idade
-p3.innerHTML="Email: "+videosJSON.email
-p4.innerHTML="Preferencia: "+videosJSON.preferencia
-p5.innerHTML="Data Do Inicio: "+videosJSON.dataInicio
-p6.innerHTML="Tipo de Assinatura: "+videosJSON.tipoAssinatura
-p7.innerHTML="Valor: "+videosJSON.valor+" R$"
+idadeUsu.innerHTML="Idade: "+videosJSON.idade
+preferenciaUsu.innerHTML="Preferencia: "+videosJSON.preferencia
+dataUso.innerHTML="Data Do Inicio: "+videosJSON.dataInicio
+tipoUsu.innerHTML="Tipo de Assinatura: "+videosJSON.tipoAssinatura
+valorUsu.innerHTML="Valor: "+videosJSON.valor+" R$"
 
 
+/*let user=localStorage.emailUser
+let bemVindo=document.querySelector("#bemVindo")
+let entrar=document.querySelector("#entrar")
+let sair=document.querySelector("#sair")
+//console.log(user.indexOf('@'))
+//console.log(user.substring(0,user.indexOf('@')))
+
+
+function msgUser() {
+    let nomeUser=user.substring(0,user.indexOf('@'))
+    bemVindo.innerHTML='Olá<b>'+ nomeUser+'</b>'
+    entrar.style.display="none"
+}*/
