012b8b6d68b4ad61e4b5cad25d2d07099f6d8923
Nikolle Kimberly da Silva <nikollekimberly@gmail.com>
2022-05-13 15:10:04 -0300

    js do carrinho

--- projeto-video/js/carrinho.js
+++ projeto-video/js/carrinho.js
@@ -9,4 +9,3 @@ let calculo2= parseInt(soma[0][0]+soma[1][0]+soma[2][0])
 document.querySelector("#total2").innerHTML = "Qtde de filmes: " + calculo2
 document.querySelector("#total").innerHTML= "Valor final: " + calculo + ",00"
 document.querySelector("#total3").innerHTML= "Valor final: " + calculo + ",00"
-
