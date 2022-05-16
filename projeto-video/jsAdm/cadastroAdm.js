e1894c8c0f8e0c8706f4b3743e7b4dabc1b298d5
Gabriel Soares <gsrdev7@gmail.com>
2022-05-16 15:27:40 -0300

    Pasta jsAdm

---
+++ projeto-video/jsAdm/cadastroAdm.js
@@ -0,0 +1,17 @@
+document.querySelector("#btContato").onclick=function(){
+    if(formC.nomeContato.value == ""){
+        alert ("Preencha o nome!")
+    }else if (formC.emailContato.value == ""){
+        alert ("Coloque seu E-mail!")
+    }else if (formC.senha.value == ""){
+        alert ("Coloque sua senha!")
+    }else if (formC.senhaC.value == ""){
+        alert ("Coloque sua senha!")}
+    else{
+         formC.submit()
+        localStorage.nomeUser=formC.nomeContato.value
+        localStorage.emailUser=formC.emailContato.value
+        localStorage.telefoneUser=formC.telefoneContato.value
+        localStorage.senhaUser=formC.senha.value
+    }
+    }
