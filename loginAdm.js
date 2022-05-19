6c22bd3d42fe585cbefc67f927255c9293baf552
Leonardo Morais <69097237+leodemorais@users.noreply.github.com>
2022-05-18 21:37:10 -0300

    Javascript para o loginAdm.html

---
+++ loginAdm.js
@@ -0,0 +1,17 @@
+//Validação de email e senha para o login do Administrador / Administrador Master
+
+let formL = document.forms.formLogin
+
+document.querySelector("#btLogin").onclick = function () {
+    if (formL.email.value == "") {
+        alert("Preencha o Email!")
+    } else if (formL.senha.value == "") {
+        alert("Coloque sua Senha!")
+    }
+    else {
+        formL.submit()
+        localStorage.emailUser = formL.email.value
+        localStorage.senhaUser = formL.senha.value
+
+    }
+}
