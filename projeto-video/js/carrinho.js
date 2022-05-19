7e6df75a9df29f24b73f1a8244ed0718369b0eef
Marianna Corrêa <102970861+mahrjDev@users.noreply.github.com>
2022-05-19 03:08:27 -0300

    novo carrinho.js

---
+++ projeto-video/js/carrinho.js
@@ -0,0 +1,65 @@
+//QUANTIDADE DE FILMES E QUANTIDADE DE VAlORES
+
+var soma = [
+    [1, 25],
+    [1, 25],
+    [1, 25]
+]
+
+let calculo = parseInt(soma[0][1] + soma[1][1] + soma[2][1])
+let calculo2 = parseInt(soma[0][0] + soma[1][0] + soma[2][0])
+
+document.querySelector("#total2").innerHTML = "Qtde de filmes: " + calculo2
+document.querySelector("#total").innerHTML = "Valor final: " + calculo + ",00"
+document.querySelector("#total3").innerHTML = "Valor final: " + calculo + ",00"
+
+let code =[123,456] ;
+function validate(coupon) { 
+   // let code =[123,456] ;
+  
+  
+ 
+   
+   if (code[0]==coupon) 
+    { 
+    window.alert("Código de cupom aceito! Clique no botão comprar!"); 
+    document.querySelector("#total3").innerHTML= (calculo -(calculo*0.10))
+    } 
+     else if (code[1]==coupon) 
+    { 
+    window.alert("Código de cupom aceito! Clique no botão comprar"); 
+    document.querySelector("#total3").innerHTML= (calculo -(calculo*0.20))
+    } 
+   else 
+    { 
+    window.alert("Sorry, The Coupon Code you entered is invalid. Please check and try again!"); 
+    } 
+   } 
+
+
+
+
+//IDENTIFICAÇÃO DO USUÁRIO 
+let user = localStorage.emailUser
+let bemVindo = document.querySelector("#bemVindo")
+let entrar = document.querySelector("#entrar")
+let sair = document.querySelector("#sair")
+let cadastro = document.querySelector("#cadastro")
+
+//SAUDAÇÃO
+function msgUser() {
+    let nomeUser = user.substring(0, user.indexOf('@'))
+    bemVindo.innerHTML = 'Olá <b>' + nomeUser + '<b/>'
+    entrar.style.display = "none"
+    cadastro.style.display = "none"
+}
+
+//ESCONDE O BOTÃO ENTRAR
+function resetUser() {
+    sair.style.display = "none"
+}
+sair.onclick = function () {
+    localStorage.removeItem("emailUser")
+    location.href = 'index.html'
+}
+localStorage.emailUser ? msgUser() : resetUser()
