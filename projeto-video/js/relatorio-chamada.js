989d22ab2ae4c21451052ec883d2bd1bd9e6e9ce
VitorAVPereira <amaralvp123@gmail.com>
2022-05-23 16:40:37 -0300

    carrinho

--- projeto-video/js/relatorio-chamada.js
+++ projeto-video/js/relatorio-chamada.js
@@ -1,26 +1,54 @@
 
-let listaUsers,listaEmail,listaTelefone,listaAssunto,idChamada
-
-idChamada=localStorage.key(idChamada)
-console.log(idChamada)
-listaUsers=localStorage.getItem(idChamada).split(',')
-listaNome=listaUsers[0]
-listaEmail=listaUsers[1]
-listaTelefone=listaUsers[2]
-listaAssunto=listaUsers[3]
-console.log(listaUsers)
-function users(){
-for (var i=0;i<listaUsers.length;i++){
-    document.querySelector("#nome1").innerHTML+="<li>" + listaNome + "</li>"
-    document.querySelector("#email1").innerHTML+="<li>" + listaEmail + "</li>"
-    document.querySelector("#telefone1").innerHTML+="<li>" + listaTelefone + "</li>"
-    document.querySelector("#assunto1").innerHTML+="<li>" + listaAssunto + "</li>"
+var i=0
+for (key in localStorage) {
+    var ch = localStorage.getItem(key)
+    
+    if(i<localStorage.length){
+        var chArray = ch.split(",")
+    i++
+    }else{
+        break
+    }
+    console.log(chArray.length)
+    // console.log(key)
+    // console.log(chArray)
+    
+    var p = document.createElement('p')
+    document.querySelector('#nome1').innerHTML += "<li>" + chArray[0] + "</li>"
+    document.querySelector('#email1').innerHTML += "<li>" + chArray[1] + "</li>"
+    document.querySelector('#telefone1').innerHTML += "<li>" + chArray[2] + "</li>"
+    document.querySelector('#assunto1').innerHTML += "<li>" + chArray[3] + "</li>"
+    
 }
-listaUsers.length>0 ? document.querySelector("#next").innerHTML=listaUsers[0] : null
-listaUsers.shift()
-users()
-}
+    let listaUsers = chArray
+    // console.log(listaUsers)
+    let listaResult = []
 
+    function users() {
+        document.querySelector("#nome1").innerHTML = ""
+        for (var i = 0; i < listaUsers.length; i++) {
+            document.querySelector("#nome1").innerHTML += "<li>" + listaUsers[i] + "</li>"
+
+            listaUsers.length > 0 ? document.querySelector("#next").innerHTML = listaUsers[0] : null
+
+
+
+
+           
+            
+        }
+        users()
+    }
+    document.querySelector("#btAtende").onclick = function () {
+        
+                   listaResult.push(listaUsers[0])
+        
+            document.querySelector("#cAtendido").innerHTML = ""
+            for (var i = 0; i < listaResult.length; i++) {
+                document.querySelector("#cAtendido").innerHTML += "<li>" + listaResult[i] + "</li>"
+            }
+            
+        }
 
 
 
