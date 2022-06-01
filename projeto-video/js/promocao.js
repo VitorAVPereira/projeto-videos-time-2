a9ec0d83f79ed8f973ca7e600626639d963ab51d
mahrjDev <mariannatodas@gmail.com>
2022-06-01 16:10:12 -0300

    refinamento geral

--- projeto-video/js/promocao.js
+++ projeto-video/js/promocao.js
@@ -66,3 +66,23 @@ for (var i = 0; i < filmes.length; i++) {
 
     galeria.appendChild(div)
 }
+let user = localStorage.emailUser
+    let bemVindo = document.querySelector("#bemVindo")
+    let entrar = document.querySelector("#entrar")
+    let sair = document.querySelector("#sair")
+    let cadastro = document.querySelector("#cadastro")
+
+    function msgUser() {
+      let nomeUser = user.substring(0, user.indexOf('@'))
+      bemVindo.innerHTML = 'Olá <b>' + nomeUser + '<b/>'
+      entrar.style.display = "none"
+      cadastro.style.display = "none"
+    }
+    function resetUser() {
+      sair.style.display = "none"
+    }
+    sair.onclick = function () {
+      localStorage.removeItem("emailUser")
+      location.href = 'index.html'
+    }
+    localStorage.emailUser ? msgUser() : resetUser()
