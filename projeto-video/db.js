c4dbf97242e6026b5e1c36975eb7e09abe4726b5
VitorAVPereira <amaralvp123@gmail.com>
2022-06-07 14:26:25 -0300

    db

---
+++ projeto-video/db.js
@@ -0,0 +1,44 @@
+async function conecta(){
+    const mysql = require("mysql2/promise")
+    const conn = await mysql.createConnection({
+        host: "localhost",
+        user: "v19",
+        password: "Sendokai123$",
+        database:"projeto_video"
+    })
+    console.log("mySQL conectado!")
+    global.connection = conn
+    return connection
+}
+
+// conecta()
+
+async function selectFilmes(){
+    const conectado = await conecta()
+    const [rows] = await conectado.query("SELECT titulo FROM filmes")
+    // console.log(rows)
+    return rows
+}
+
+// selectFilmes()
+
+// async function selectFilmes1(){
+//     const conectado = await conecta()
+//     const [rows] = await conectado.query("SELECT titulo,sinopse FROM filmes")
+//     // console.log(rows)
+//     return rows
+// }
+
+// selectFilmes1()
+
+// async function selectFilmes2(){
+//     const conectado = await conecta()
+//     const [rows] = await conectado.query("SELECT titulo,genero,ano,classificacao,sinopse FROM filmes")
+//     // console.log(rows)
+//     return rows
+// }
+
+// selectFilmes2()
+module.exports = {selectFilmes}
+// module.exports = {selectFilmes1}
+// module.exports = {selectFilmes2}
