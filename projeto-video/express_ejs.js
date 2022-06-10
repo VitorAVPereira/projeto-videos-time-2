58f5c688f1c85ebfe671b192a429d47f86eea943
VitorAVPereira <amaralvp123@gmail.com>
2022-06-10 13:55:05 -0300

    db de usuario novo

--- projeto-video/express_ejs.js
+++ projeto-video/express_ejs.js
@@ -2,14 +2,17 @@
    const express = require('express')
    const app = express()
    const db = require("./db.js")
+   const bodyParser = require("body-parser")
    const port = 8000
    const url = require ("url")
 app.set("view engine","ejs")
 
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 app.use(express.static('projeto-videos-time-2'))
 app.use("/imagens",express.static("imagens"))
 app.use("/js",express.static("js"))
-
+app.use("/adm", express.static("adm"))
 const consulta = await db.selectFilmes()
 console.log(consulta[0])
 
@@ -31,6 +34,18 @@ app.get("/index",(req, res) => {
       galeria:consulta})
 })
  
+app.post("/cadastro",async (req,res)=>{
+   const info = req.body
+   await db.insertCadastro({
+       nome:info.nomeContato,
+       email:info.emailContato,
+       telefone:info.telefoneContato,
+       senha:info.senha,
+       conf_senha:info.senhaC
+   })      
+   res.redirect('/index')
+})
+
 app.get("/cadastro",(req, res) => {
     
    res.render(`cadastro`)
@@ -67,11 +82,11 @@ app.get("/produto",(req, res) => {
    filmes:consulta})
 })
 
-app.get("/promocao",(req, res) => {
-    
-   res.render(`promocao`),{
-      promo:consulta
-   }
+app.get("/promocao",async(req, res) => {
+   const consultaPromo = await db.selectPromo()
+   res.render(`promocao`,{
+         filmes:consulta,
+         galeria:consultaPromo})
    
 })
 
@@ -79,6 +94,24 @@ app.get("/singleprefer",(req, res) => {
     
    res.render(`singleprefer`)
    
+})
+
+app.get("/atualiza-promo",async(req,res)=>{
+         
+   let infoUrl = req.url
+   let urlProp = url.parse(infoUrl, true)
+   let q = urlProp.query
+   // let qs = url.parse(req.url,true).query
+   await db.updatePromo(q.promo,q.id)//localhost:8080/atualiza-promo?promo=1&id=8
+   res.send("<h2> Lista de promocoes atualizada </h2><a href='/promocao'>voltar</a>")
+})
+
+
+app.get("/upd-promo", (req, res) => {
+   res.render(`adm/atualiza-promo`, {
+       filmes: consulta,
+       galeria: consulta
+   })
 })
 
 app.get("/single",async(req, res) => {
