4d66e4231651b80abf1be6cd1b3860a01a67c1cb
Vitor Pereira <102987733+VitorAVPereira@users.noreply.github.com>
2022-06-22 08:06:36 -0300

    Update contato.js

--- projeto-video/js/contato.js
+++ projeto-video/js/contato.js
@@ -1,3 +1,30 @@
+
+
+let user = localStorage.emailUser
+let bemVindo = document.querySelector("#bemVindo")
+let entrar = document.querySelector("#entrar")
+let sair = document.querySelector("#sair")
+let cadastro = document.querySelector("#cadastro")
+
+function msgUser() {
+    let nomeUser = user.substring(0, user.indexOf('@'))
+    bemVindo.innerHTML = 'Olá, <b>' + nomeUser + '<b/>'
+    entrar.style.display = "none"
+    cadastro.style.display = "none"
+}
+function resetUser() {
+    sair.style.display = "none"
+}
+sair.onclick = function () {
+    localStorage.removeItem("emailUser")
+
+    location.href = 'index.html'
+
+}
+
+localStorage.emailUser ? msgUser() : resetUser()
+
+//////////////////////////////////////////////////////////////////////
 //Validação do Formulário de Contato//
 var formC=document.forms.formContato
 document.querySelector('#envia').onclick=function(){
