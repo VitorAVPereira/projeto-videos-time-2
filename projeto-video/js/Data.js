59b76771e2440cf2a1faa9d05d9dea134d2c68cb
smith <fellipev72@gmail.com>
2022-06-28 17:18:33 -0300

    Single dinâmica

---
+++ projeto-video/js/Data.js
@@ -0,0 +1,55 @@
+let shopItemsData = [
+    {
+        id: "ferro",
+        nome: 'Homem de Ferro',
+        img: '../imagens/Homemdeferro.jpeg',
+        quantidade: 0,
+        preco:9.9
+    },
+    {
+        id: "hulk",
+        nome: 'O Incrivel Hulk',
+        img: '../imagens/Hulk.jpeg',
+        quantidade: 0,
+        preco:9.90
+    },
+    {
+        id: "ferro2",
+        nome: 'Homem de Ferro 2',
+        img: '../imagens/HomemDeFerro2.jpeg',
+        quantidade: 0,
+        preco:9.90
+    },
+    {
+        id: "thor",
+        nome: 'Thor',
+        img: '../imagens/Thor1.jpeg',
+        quantidade: 0,
+        preco:9.9
+    },
+    {
+        id: "capitao",
+        nome: 'Capitão America - O Primeiro Vingador',
+        img: '../imagens/capitao.jpeg',
+        quantidade: 0,
+        preco:9.90
+    },    {
+        id: "vingadores",
+        nome: 'Os Vingadores',
+        img: '../imagens/Vingadores.jpeg',
+        quantidade: 0,
+        preco:9.90
+    },{
+        id: "thor2",
+        nome: 'Thor 2 - O mundo Sombrio',
+        img: '../imagens/Thor2.jpeg',
+        quantidade: 0,
+        preco:9.90
+    },{
+        id: "capit2",
+        nome: 'Capitão America - Soldado invernal',
+        img: '../imagens/SoldadoInvernal.jpeg',
+        quantidade: 0,
+        preco:9.90
+    },
+  ];
